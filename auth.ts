e089c5e69f4fcfc155d6ef3042c04b8ae4cc0144
akiyoshiyamamoto <64624494+akiyoshiyamamoto@users.noreply.github.com>
2026-02-24 22:02:45 +0900

    fix

--- auth.ts
+++ auth.ts
@@ -13,7 +13,7 @@ async function getUser(email: string): Promise<User | undefined> {
         const user = await sql<User[]>`SELECT * FROM users WHERE email=${email}`;
         return user[0];
     } catch (error) {
-        console.error('Failed to fetch user:'. error);
+        console.error('Failed to fetch user:', error);
         throw new Error('Failed to fetch user.');
     }
 };
